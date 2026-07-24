38edc2b10917acfca80fb33f7626a417edf6a2de
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-07-24 17:18:20 +0200

    nix: move package.nix to generated gepetto/nix

--- flake.nix
+++ flake.nix
@@ -6,9 +6,23 @@
   outputs =
     inputs:
     inputs.gepetto.lib.mkFlakoboros inputs (
-      { ... }:
+      { lib, ... }:
       {
-        rosPackages.hpp-rviz = ./package.nix;
+        rosOverrideAttrs.hpp-rviz = {
+
+          src = lib.fileset.toSource {
+            root = ./.;
+            fileset = lib.fileset.unions [
+              ./CMakeLists.txt
+              ./doc
+              ./package.xml
+              ./pyproject.toml
+              ./rviz_common_plugins.xml
+              ./src
+            ];
+          };
+
+        };
       }
     );
 }
